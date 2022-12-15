0adeb459e2052c0a8240ccbc5c850f326ff6ae02
GitHub Action <action@github.com>
2022-12-15 03:32:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhangwei2811/qidiancopilot/MyBundle.kt
+++ src/main/kotlin/com/github/zhangwei2811/qidiancopilot/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zhangwei2811.qidiancopilot
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zhangwei2811/qidiancopilot/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zhangwei2811/qidiancopilot/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhangwei2811.qidiancopilot.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhangwei2811.qidiancopilot.MyBundle
 
 class MyProjectService(project: Project) {
 
